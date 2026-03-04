8e24ddbbac3352acaa782ece18cb3e31c2949bf2
chenhaochuan <haochuan.chen@itronsvs.com>
2026-03-04 16:15:22 +0800

    feat: 新增NPC章节系统

--- script/progress_bar_level.gd
+++ script/progress_bar_level.gd
@@ -3,14 +3,16 @@ extends TextureProgressBar
 
 @export var Name_Label: Label
 @export var Name_start: String
+@export var current_progress: int
 
 func _ready() -> void:
-	set_progress(20)
+	set_progress(current_progress)
 	pass
 
 # 设置当前的进度值(绝对量) 
 func set_progress(progress:int) -> void:
 	self.value = progress
 	var display_text = "%.2f%%" % progress
+	print(display_text, "--- ", progress)
 	Name_Label.text = Name_start + " " + display_text
 	pass
